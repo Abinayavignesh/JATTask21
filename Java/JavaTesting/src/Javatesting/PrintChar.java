f113bfa861338362d6a24d56b6905b7b1efdef52
Abinayavignesh <Abinayavignesh91@gmail.com>
2024-03-26 16:04:42 +0530

    My Commit

---
+++ Java/JavaTesting/src/Javatesting/PrintChar.java
@@ -0,0 +1,13 @@
+package Javatesting;
+
+public class PrintChar {
+
+	public static void main(String[] args) {
+    char alp;
+    for(alp='A';alp<='Z';alp++) {
+    	System.out.println(alp +"");
+    }
+
+	}
+
+}
